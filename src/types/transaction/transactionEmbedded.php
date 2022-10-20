d2b533b1d01b6bea068b03fa016794e624dc5530
Andrew Sauder <andrewsauder@gmail.com>
2022-10-19 20:02:31 -0400

    update transaction embedded fields

--- src/types/transaction/transactionEmbedded.php
+++ src/types/transaction/transactionEmbedded.php
@@ -2,13 +2,13 @@
 
 namespace elite42\trackpms\types\transaction;
 
-
 class transactionEmbedded
 	extends
 	\andrewsauder\jsonDeserialize\jsonDeserialize {
 
-	public ?\elite42\trackpms\types\transaction          $parent    = null;
-	public ?\elite42\trackpms\types\maintenanceWorkOrder $workOrder = null;
-	public ?\elite42\trackpms\types\unit                 $unit      = null;
+	public ?\elite42\trackpms\types\transaction          $parent      = null;
+	public ?\elite42\trackpms\types\maintenanceWorkOrder $workOrder   = null;
+	public ?\elite42\trackpms\types\unit                 $unit        = null;
+	public ?\elite42\trackpms\types\reservation          $reservation = null;
 
 }
