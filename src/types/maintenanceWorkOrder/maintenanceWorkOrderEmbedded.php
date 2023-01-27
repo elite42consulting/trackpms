75e2d8ae4f8b2ac94c84eadbaf8da739da874fc7
Andrew Sauder <andrewsauder@gmail.com>
2023-01-26 21:39:32 -0500

    add embedded fields

--- src/types/maintenanceWorkOrder/maintenanceWorkOrderEmbedded.php
+++ src/types/maintenanceWorkOrder/maintenanceWorkOrderEmbedded.php
@@ -2,13 +2,13 @@
 
 namespace elite42\trackpms\types\maintenanceWorkOrder;
 
-
 class maintenanceWorkOrderEmbedded
 	extends
 	\andrewsauder\jsonDeserialize\jsonDeserialize {
 
 	public ?\elite42\trackpms\types\unit   $unit   = null;
-
 	public ?\elite42\trackpms\types\vendor $vendor = null;
+	public ?\elite42\trackpms\types\owner  $owner  = null;
+	public ?\elite42\trackpms\types\user   $user   = null;
 
 }
